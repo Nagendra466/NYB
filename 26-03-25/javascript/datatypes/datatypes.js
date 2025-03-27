aa4d18e1438ffd9bdefa1eaab8b402d8495a24f4
nagendra <nagendra.s@navayuvabharatinfotech.com>
2025-03-27 16:01:50 +0530

    datatypes

---
+++ 26-03-25/javascript/datatypes/datatypes.js
@@ -0,0 +1,9 @@
+var A="Ferrari"
+
+let B=10
+
+const C=false
+
+console.log(A)
+
+console.log(B)
---
+++ 26-03-25/javascript/datatypes/mar27.js
@@ -0,0 +1,22 @@
+// var
+var A="this is N@gendr@;"
+
+var B='Lamborgini'
+
+console.log(A)
+console.log(B)
+
+//let
+
+let P=15
+
+let Q=P
+
+console.log(A)
+console.log(B)
+
+//null
+
+var k=null
+
+console.log(k)
